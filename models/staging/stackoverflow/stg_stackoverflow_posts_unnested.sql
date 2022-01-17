abe630cf2811bb3d07107cd13918e9c185d185f8
gparry49@gmail.com <gparry49@gmail.com>
2022-01-17 23:52:09 +0000

    Unnest Tags

---
+++ models/staging/stackoverflow/stg_stackoverflow_posts_unnested.sql
@@ -0,0 +1,13 @@
+with convert_tags_to_array as (
+    select 
+        *, 
+        SPLIT(tags,'|') as array_tags 
+    from
+        {{ ref('stg_stackoverflow_posts') }}
+)
+
+select
+    GENERATE_UUID() as id_surrogate_key,
+    *
+from convert_tags_to_array,
+UNNEST(array_tags) as single_tag
